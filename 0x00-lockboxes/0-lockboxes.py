4da805bda703d82dad4da49d154cb28e194b806a
yony brinez <arleybri18@gmail.com>
2020-07-19 17:00:09 -0500

    Add case empty boxes, add to opened

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+
+
+
 def canUnlockAll(boxes):
     '''Method that validate if all boxes were opened
        Use set for save opened boxes, unique values
        Iterate over keys array, and add new keys to opened set
-       Retur True if all boxes opened, else False 
+       Return True if all boxes opened, else False 
     '''
     opened = {0}
     keys = boxes[0]
+    for x in range(len(boxes)):
+        if len(boxes[x]) == 0:
+            opened.add(x)
+
     for key in keys:
         if key not in opened:
           keys += boxes[key]
