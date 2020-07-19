8efe539cb32d32790b49bc11e30ea112c42f73d5
yony brinez <arleybri18@gmail.com>
2020-07-19 18:43:29 -0500

    autopep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 
-
 def canUnlockAll(boxes):
     '''Method that validate if all boxes were opened
        Use set for save opened boxes, unique values
        Iterate over keys array, and add new keys to opened set
-       Return True if all boxes opened, else False 
+       Return True if all boxes opened, else False
     '''
     opened = {0}
     keys = boxes[0]
@@ -16,6 +15,6 @@ def canUnlockAll(boxes):
 
     for key in keys:
         if key not in opened:
-          keys += boxes[key]
-          opened.add(key)
+            keys += boxes[key]
+            opened.add(key)
     return len(boxes) == len(opened)
--- 0x00-lockboxes/main_0.py
+++ 0x00-lockboxes/main_0.py
@@ -10,3 +10,33 @@ print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
+
+# my cases test
+print('### My cases ###')
+
+boxes = [[]]
+print(canUnlockAll(boxes)) #>>> True
+
+boxes = [[], [1]]
+print(canUnlockAll(boxes)) #>>> False
+
+boxes = [[0], [1]]
+print(canUnlockAll(boxes)) #>>> False
+
+boxes = [[1], []]
+print(canUnlockAll(boxes)) #>>> True
+
+boxes = [[1], [3], [], [2]]
+print(canUnlockAll(boxes)) #>>> True
+
+boxes = [[1], [1], [2]]
+print(canUnlockAll(boxes)) #>>> False
+
+boxes = [[],[5],[666],[],[],[1,2,3,4,5]]
+print(canUnlockAll(boxes)) #>>> False -> True
+
+boxes = [[], [], [], [], []]
+print(canUnlockAll(boxes)) #>>> True
+
+boxes = [[4],[],[],[],[0]]
+print(canUnlockAll(boxes)) #>>> True
